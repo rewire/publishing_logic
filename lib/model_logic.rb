8573dd22a46898f2fdf22d5f987463b12cff5e7e
Tom ten Thij <git@tomtenthij.nl>
2009-12-10 12:24:34 +0000

    published named scope, including newest and oldest methods on it

--- lib/model_logic.rb
+++ lib/model_logic.rb
@@ -1,9 +1,34 @@
 module PublishingLogic
   module ModelLogic
+    PUBLISHED_CONDITIONS = <<EOT
+publishing_enabled = true AND
+(published_until is null or published_until > ?) AND
+(published_at is null or published_at < ?)
+EOT
+
     def published?
       return false if published_at && Time.now < published_at
       return false if published_until && Time.now > published_until
       publishing_enabled?
     end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+      base.instance_eval do
+        named_scope :published, lambda {{ :conditions => [PUBLISHED_CONDITIONS, Time.now.utc, Time.now.utc] }} do
+          def newest
+            find(:first, :order => "published_at DESC")
+          end
+
+          def oldest
+            find(:last, :order => "published_at DESC")
+          end
+        end
+      end
+    end
+
+    module ClassMethods
+    end
+
   end
 end
--- spec/lib/model_logic_spec.rb
+++ spec/lib/model_logic_spec.rb
@@ -41,4 +41,69 @@ describe PublishingLogic::ModelLogic do
       end
     end
   end
+
+  describe "published named scope" do
+    it "should include published objects" do
+      fun_item = Factory.create(:fun_item, :publishing_enabled => true)
+      FunItem.published.should == [fun_item]
+    end
+
+    it "should not include any unpublished objects" do
+      fun_item = Factory.create(:fun_item, :publishing_enabled => false)
+      FunItem.published.should be_empty
+    end
+
+    # it "should not expose a published episode published an hour ago" do
+    #   article = Factory.create(:episode, :is_hidden => false, :published_at => 1.hour.from_now, :published_until => nil)
+    #   Episode.published.should == []
+    # end
+
+    it "should not include objects with a published_until in the past" do
+      Factory.create(:fun_item,
+                     :publishing_enabled => true,
+                     :published_until => 5.seconds.ago)
+      FunItem.published.should be_empty
+    end
+
+    it "should not include objects with a published_at in the future" do
+      Factory.create(:fun_item,
+                     :publishing_enabled => true,
+                     :published_at => 5.seconds.from_now)
+      FunItem.published.should be_empty
+    end
+
+    it "should get a new Time.now for each invocation of the named scope" do
+      item = Factory.create(:fun_item,
+                            :publishing_enabled => true,
+                            :published_until => 10.days.from_now)
+      mock_now = mock('now', :utc => 20.days.from_now, :to_f => 0)
+      Time.stub(:now).and_return mock_now
+      FunItem.published.should be_empty
+    end
+
+    it "should use the utc of the current time" do
+      # Make sure utc is used, which is hard to test as a behaviour
+      mock_now = mock('now')
+      Time.stub(:now).and_return mock_now
+      mock_now.should_receive(:utc).twice
+      FunItem.published
+    end
+
+    describe "newest" do
+      it "should be the most recently published object" do
+        object2 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 2.days.ago)
+        object1 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 1.days.ago)
+        object3 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 3.days.ago)
+        FunItem.published.newest.should == object1
+      end
+    end
+    describe "oldest" do
+      it "should be the object published the longest ago" do
+        object2 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 2.days.ago)
+        object1 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 1.days.ago)
+        object3 = Factory.create(:fun_item, :publishing_enabled => true, :published_at => 3.days.ago)
+        FunItem.published.oldest.should == object3
+      end
+    end
+  end
 end
