f21c5510c6201101e7a0daddc433be58d1641900
Tom ten Thij <git@tomtenthij.nl>
2009-12-10 10:55:04 +0000

    published? method implemented

--- lib/model_logic.rb
+++ lib/model_logic.rb
@@ -1,7 +1,9 @@
 module PublishingLogic
   module ModelLogic
     def published?
-      true
+      return false if published_at && Time.now < published_at
+      return false if published_until && Time.now > published_until
+      publishing_enabled?
     end
   end
 end
--- spec/lib/model_logic_spec.rb
+++ spec/lib/model_logic_spec.rb
@@ -9,8 +9,36 @@ Factory.define :fun_item do |c|
 end
 
 describe PublishingLogic::ModelLogic do
-  it "should be published by default" do
-    # Factory.create(:fun_item, :publishing_enabled => true).should be_published
-    Factory.create(:fun_item).should be_published
+  describe "published?" do
+    describe "with publishing enabled" do
+      it "should be published by default" do
+        Factory.create(:fun_item,
+                       :publishing_enabled => true,
+                       :published_at => nil,
+                       :published_until => nil).should be_published
+      end
+
+      it "should not be published if the published_at datetime is in the future" do
+        Factory.create(:fun_item,
+                       :publishing_enabled => true,
+                       :published_at => 5.seconds.from_now,
+                       :published_until => nil).should_not be_published
+      end
+
+      it "should not be published if the published_until datetime is in the past" do
+        Factory.create(:fun_item,
+                       :publishing_enabled => true,
+                       :published_at => nil,
+                       :published_until => 5.seconds.ago).should_not be_published
+      end
+    end
+    describe "with publishing disabled" do
+      it "should not be published" do
+        Factory.create(:fun_item,
+                       :publishing_enabled => false,
+                       :published_at => 1.days.ago,
+                       :published_until => 10.days.from_now).should_not be_published
+      end
+    end
   end
 end
