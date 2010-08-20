f4003a56e29713565f4739095376e5e62f0bdfcc
Murray Steele <muz@h-lame.com>
2010-08-20 15:59:56 +0100

    Remove the double negative of no_published_until in the code, but leave it as the CLI option.

--- rails_generators/publishing_logic_fields/publishing_logic_fields_generator.rb
+++ rails_generators/publishing_logic_fields/publishing_logic_fields_generator.rb
@@ -1,5 +1,5 @@
 class PublishingLogicFieldsGenerator < Rails::Generator::NamedBase
-  default_options :no_published_until => false, :skip_admin_form => false
+  default_options :use_published_until_field => true, :skip_admin_form => false
 
   def initialize(args, options)
     super(args, options)
@@ -10,13 +10,13 @@ class PublishingLogicFieldsGenerator < Rails::Generator::NamedBase
       class_name.camelize.constantize # Raise an error if model does not yet exist
       m.migration_template 'db/migrate/add_publishing_logic_fields.rb.erb', 'db/migrate', :assigns => {
         :migration_name => "AddPublishingLogicFieldsTo#{class_name.pluralize.gsub(/::/, '')}",
-        :no_published_until => options[:no_published_until]
+        :use_published_until_field => options[:use_published_until_field]
       }, :migration_file_name => "add_publishing_logic_fields_to_#{file_path.gsub(/\//, '_').pluralize}"
       unless options[:skip_admin_form]
         m.template 'app/views/publishing_logic_fields.html.erb',
                    File.join('app', 'views', 'admin', plural_name, "_publishing_logic_fields.html.erb"),
                    :assigns => {
-                     :no_published_until => options[:no_published_until]
+                     :use_published_until_field => options[:use_published_until_field]
                    }
       end
     end
@@ -27,7 +27,7 @@ class PublishingLogicFieldsGenerator < Rails::Generator::NamedBase
       opt.separator ''
       opt.separator 'Options:'
       opt.on("--no-published-until",
-             "Don't add the published_until field to this model") { |v| options[:no_published_until] = true }
+             "Don't add the published_until field to this model") { |v| options[:use_published_until_field] = false }
       opt.on("--skip-admin-form",
              "Don't generate the admin form partial for this model") { |v| options[:skip_admin_form] = true }
     end
