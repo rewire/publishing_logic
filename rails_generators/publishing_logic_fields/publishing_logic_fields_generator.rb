ac779d17b3080a7bc68a09f6f9d8a876983a9c40
Murray Steele <muz@h-lame.com>
2010-08-20 15:34:39 +0100

    Fix the generators to support: a) not rendering the view b) not adding the published_until field.

--- rails_generators/publishing_logic_fields/publishing_logic_fields_generator.rb
+++ rails_generators/publishing_logic_fields/publishing_logic_fields_generator.rb
@@ -1,4 +1,6 @@
 class PublishingLogicFieldsGenerator < Rails::Generator::NamedBase
+  default_options :no_published_until => false, :skip_admin_form => false
+
   def initialize(args, options)
     super(args, options)
   end
@@ -7,9 +9,26 @@ class PublishingLogicFieldsGenerator < Rails::Generator::NamedBase
     record do |m|
       class_name.camelize.constantize # Raise an error if model does not yet exist
       m.migration_template 'db/migrate/add_publishing_logic_fields.rb.erb', 'db/migrate', :assigns => {
-        :migration_name => "AddPublishingLogicFieldsTo#{class_name.pluralize.gsub(/::/, '')}"
+        :migration_name => "AddPublishingLogicFieldsTo#{class_name.pluralize.gsub(/::/, '')}",
+        :no_published_until => options[:no_published_until]
       }, :migration_file_name => "add_publishing_logic_fields_to_#{file_path.gsub(/\//, '_').pluralize}"
-      m.template 'app/views/publishing_logic_fields.html.erb', File.join('app', 'views', 'admin', plural_name, "_publishing_logic_fields.html.erb")
+      unless options[:skip_admin_form]
+        m.template 'app/views/publishing_logic_fields.html.erb',
+                   File.join('app', 'views', 'admin', plural_name, "_publishing_logic_fields.html.erb"),
+                   :assigns => {
+                     :no_published_until => options[:no_published_until]
+                   }
+      end
     end
   end
+
+  protected
+    def add_options!(opt)
+      opt.separator ''
+      opt.separator 'Options:'
+      opt.on("--no-published-until",
+             "Don't add the published_until field to this model") { |v| options[:no_published_until] = true }
+      opt.on("--skip-admin-form",
+             "Don't generate the admin form partial for this model") { |v| options[:skip_admin_form] = true }
+    end
 end
